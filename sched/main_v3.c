c95f4aca61d44e55c2f19521d2ebe2a1fc263183
GaryChenMesser <gary9192631770@gmail.com>
2018-05-14 04:18:29 +0800

    add more comments and remove unused lines

--- sched/main_v3.c
+++ sched/main_v3.c
@@ -34,11 +34,6 @@
 int POLICY[4] = {1, 2, 5, 6};
 typedef enum { CLOCK, REMAIN } GLOBAL_V;
 
-// shared global variables:
-unsigned long _c = 0, _r = 0;
-unsigned long *_clock = &_c, *_remain =&_r;
-sem_t *mutex, *wrt, *cond1, *cond2;
-
 int main(){
 	#ifdef DEBUG
 	// disable buffering on stdout, which make printf in order.
@@ -62,14 +57,15 @@ int main(){
 		R_index[i] = T_index[i] = i;
 	
 	// restrict to one cpu usage
-	cpu_set_t mask;
+	cpu_set_t mask, mask1;
+
 	CPU_ZERO(&mask);
 	CPU_SET(0, &mask);
 	
-	cpu_set_t mask1;
 	CPU_ZERO(&mask1);
 	CPU_SET(1, &mask1);
-		
+
+	// restrict the main process to execute on cpu 1
 	if(sched_setaffinity(0, sizeof(cpu_set_t), &mask1)){
 		printf("sched_setaffinity error: %s\n", strerror(errno));
 		exit(1);
@@ -78,7 +74,7 @@ int main(){
 	// set the scheduler
 	struct sched_param param;
 	param.sched_priority = sched_get_priority_max(policy(S[0]));
-	
+	// set once and all the child process will inherit the settings	
 	if(sched_setscheduler(0, policy(S[0]), &param)){
 		printf("sched_setscheduler error: %s\n", strerror(errno));
 		exit(1);
@@ -98,16 +94,19 @@ int main(){
 	struct timezone zone;
 	
 	for(0; i < N; ++i){
+		
+		// wait until the first child to be forked
 		while(local_clock != R[i]){
 			wait_one_unit;
 			++local_clock;
 		}
 		
 		printf("%s forks!\n", P[R_index[i]]);
-		gettimeofday(&start, &zone);
 		pid = fork();
+		gettimeofday(&start, &zone);
+		
 		if(!pid){
-			// restrict to one cpu usage
+			// restrict all child processes to be executed on cpu 0
 			printf("%s %d\n", P[R_index[i]], getpid());
 			if(sched_setaffinity(0, sizeof(cpu_set_t), &mask)){
 				printf("sched_setaffinity error: %s\n", strerror(errno));
@@ -124,14 +123,12 @@ int main(){
 	goto PARENT;
 	
 CHILD:
-	//printf("%s runs!\n", P[R_index[i]]);
 	for(unsigned long _i = 0; _i < T[T_inverse[R_index[i]]]; ++_i){
 		wait_one_unit;
-		//printf("%s runs %d!\n", P[R_index[i]], _i);
 	}
 	//wait_unit(T[T_inverse[R_index[i]]]);
-	//gettimeofday(&end, &zone);
-	//printk("[Project1] %s %.9f %.9f", P[R_index[i]], start.tv_sec + start.tv_usec / 10^6, end.tv_sec + end.tv_usec / 10^6);
+	
+	gettimeofday(&end, &zone);
 	printf("%s terminates!\n", P[R_index[i]]);
 	syscall(334, P[R_index[i]], start, end);
 	exit(0);
@@ -142,7 +139,7 @@ PARENT:
 	sem_unlink("/mymutex");
 	sem_unlink("/mywrt");
 	sem_unlink("/mycond");
-printf("main terminated.\n");
+	printf("main terminated.\n");
 	exit(0);
 }
 
