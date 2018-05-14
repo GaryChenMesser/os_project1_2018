e6064832b0fd65e4fcbc11266461e570b682a8d7
GaryChenMesser <gary9192631770@gmail.com>
2018-05-14 09:09:37 +0000

    for merge

--- sched/syscall.c
+++ sched/syscall.c
@@ -3,13 +3,15 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <time.h>
+#include <errno.h>
+#include <sys/time.h>
 
 int main(int argc, char** argv){
-    struct timespec start, end;
+    struct timeval start, end;
     printf("%d\n", syscall(333, 1, 1));
-    printf("%d\n", syscall(335, &start));
-    printf("%d\n", syscall(335, &end));
-
-    printf("%d\n", syscall(334, "p1", start, end));
+//    printf("%d\n", syscall(335, &start));
+//    printf("%d\n", syscall(335, &end));
+//printf("Error: %s\n", strerror(errno));
+    printf("%d\n", syscall(334, "p1", &start, &end));
     return 0;
 }
